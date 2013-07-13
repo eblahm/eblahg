76125c5fc9cfe051f159aad292eebd8b19f9b36a
eblahm <matthew.c.halbe@gmail.com>
2013-07-12 21:27:41 -0400

    still working

--- eblahg/sync.py
+++ eblahg/sync.py
@@ -19,35 +19,44 @@ from eblahg.utility import dropbox_api, dropox_info
 
 class main(webapp2.RequestHandler):
     def get(self):
-        # scenario 1 -> new user
+        v = {}
         db_info = dropox_info.get_by_key_name('DROPBOX_SECRETS')
         if db_info == None:
+            # scenario 1 -> new user
             di = dropox_info(
                 key_name='DROPBOX_SECRETS',
                 app_key='oakhf7vqc9cqy6d',
                 app_secret='gqod403qntsb51g'
             ) # initialize
             di.put() # initialize
-            render.page(self, '/templates/base.html', values={'dumb_content': 'please enter app key or secret incorrect'})
-
-        # scenario 2 -> oauth handshake fail
-        if bool(self.request.get('error')) is True:
-            render.page(self, '/templates/base.html', values={'dumb_content': 'app key or secret incorrect'})
-
-        DB = dropbox_api()
-        pics_meta = DB.request_meta('/pics')
-        if 'error' in pics_meta:
-            if self.request.get('attempt') != '2':
-                # scenario 2 -> first attempt at oauth after inputing app key and secret
-                # redirect to handshake
-                self.redirect('/sync/login')
-            else:
-                # scenario 3 -> oauth handshake fail, redirect
-                self.redirect('/dropbox?error=True')
+            render.page(self, '/templates/main/dropbox.html', values=v)
         else:
-            # scenario 4 -> user is authorized
-            content = pics_meta
-            render.page(self, '/templates/base.html', values={'dumb_content': content})
+            v.update({'app_key': db_info.app_key, 'app_secret': db_info.app_secret})
+            DB = dropbox_api()
+            pics_meta = DB.request_meta('/pics')
+            if 'error' in pics_meta:
+                if self.request.get('attempt') != '2':
+                    # scenario 2 -> first attempt at oauth after inputing app key and secret
+                    # redirect to handshake
+                    self.redirect('/sync/login')
+                else:
+                    # scenario 3 -> oauth handshake fail
+                    v.update({'wrong_key': True})
+                    render.page(self, '/templates/main/dropbox.html', values=v)
+            else:
+                # scenario 4 -> user is authorized
+                content = pics_meta
+                render.page(self, '/templates/main/dropbox.html', values=v)
+    def post(self):
+        di = dropox_info.get_by_key_name('DROPBOX_SECRETS')
+        di.app_key = self.request.get('app_key')
+        di.app_secret = self.request.get('app_secret')
+        di.save()
+        self.redirect('/sync/login')
+
+def sync_datastore():
+    dropbox = dropbox_api()
+
 
 class handshake(webapp2.RequestHandler):
     def get(self, mode="login"):
@@ -69,7 +78,7 @@ class handshake(webapp2.RequestHandler):
             # restart dropbox client with access token and secret
             dropbox = dropbox_api()
             # create the default file structure
-            paths = ['/sidebar']
+            paths = ['/pics']
             for p in paths:
                 params = {'root': 'sandbox', 'path': p}
                 api_request = dropbox.create_folder(params)
