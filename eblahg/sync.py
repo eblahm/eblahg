13fdf6c7c2d2092258db6adc54b14b1b7e9b9f3b
Matt Halbe <matthew.c.halbe@gmail.com>
2013-07-13 08:17:05 -0400

    improvements to sync dance

--- eblahg/sync.py
+++ eblahg/sync.py
@@ -1,43 +1,36 @@
 import fix_path
 fix_path.fix()
-import config
-import urllib
-import json
-import re
 from datetime import datetime
+
 import webapp2
 from google.appengine.ext import db
-from google.appengine.api import images
 from google.appengine.api import taskqueue
 from google.appengine.api import memcache
-from google.appengine.api import urlfetch
 import oauth
-import logging
+
 from eblahg import models, render
 from eblahg.utility import dropbox_api, dropox_info, upload_pic
+from externals.pytz.gae import pytz
 
 
 def sync_datastore():
     dropbox = dropbox_api()
     pics_meta = dropbox.request_meta('/pics')
+    dstore = {}
+    for p in models.pics.all():
+        dstore[p.path] = p.rev
 
-    if 'error' in pics_meta:
-        assert False
-    else:
-        dstore = {}
-        for p in models.pics.all():
-            dstore[p.path] = p.rev
+    for remote in pics_meta.get('contents', []):
+        dstore_rev = dstore.get(remote['path'], "")
+        if dstore_rev != remote['rev']:
+            upload_pic(remote['path'], remote['rev'])
+            if dstore_rev != "":
+                dstore.pop(remote['path'])
 
-        for remote in pics_meta['contents']:
-            dstore_rev = dstore.get(remote['path'], "")
-            if dstore_rev != remote['rev']:
-                upload_pic(remote['path'], remote['rev'])
-                if dstore_rev != "":
-                    dstore.pop(remote['path'])
+    for deleted in dstore:
+        rec = models.pics.get_by_key_name(deleted)
+        rec.delete()
 
-        for deleted in dstore:
-            rec = models.pics.get_by_key_name(deleted)
-            rec.delete()
     return True
 
 html_template = '/templates/main/dropbox.html'
@@ -47,44 +40,43 @@ class main(webapp2.RequestHandler):
         v = {}
         db_info = dropox_info.get_by_key_name('DROPBOX_SECRETS')
         if db_info == None:
-            # scenario 1 -> new user
+            # -> new user
             di = dropox_info(
                 key_name='DROPBOX_SECRETS',
-                app_key='oakhf7vqc9cqy6d',
-                app_secret='gqod403qntsb51g'
             ) # initialize
             di.put() # initialize
-            render.page(self, html_template, values=v)
         else:
+            # -> returning user
             v.update({'app_key': db_info.app_key, 'app_secret': db_info.app_secret})
-            DB = dropbox_api()
-            pics_meta = DB.request_meta('/pics')
-            if 'error' in pics_meta:
-                if self.request.get('attempt') != '2':
-                    # scenario 2 -> first attempt at oauth after inputing app key and secret
-                    # redirect to handshake
-                    self.redirect('/sync/login')
-                else:
-                    # scenario 3 -> oauth handshake fail
-                    v.update({'error': True})
-                    render.page(self, html_template, values=v)
-            else:
-                # scenario 4 -> user is authorized
-                content = pics_meta
-                render.page(self, html_template, values=v)
+        render.page(self, html_template, values=v)
+
     def post(self):
         v = {}
         di = dropox_info.get_by_key_name('DROPBOX_SECRETS')
         di.app_key = self.request.get('app_key')
         di.app_secret = self.request.get('app_secret')
         di.save()
-        v.update({'app_key': di.app_key, 'app_secret': di.app_secret})
-        sync_datastore()
-        v.update({'sync': True})
-        render.page(self, html_template, values=v)
-        # except:
-        #     v.update({'error': True})
-        #     render.page(self, html_template, values=v)
+        try:
+            DB = dropbox_api()
+        except:
+            v['error'] = True
+            render.page(self, html_template, values=v)
+
+        if not v.get('error', False):
+            meta = DB.request_meta('/pics')
+            if 'error' in meta:
+                # -> attempt oauth
+                # redirect to handshake
+                self.redirect('/sync/login')
+            else:
+                # -> user is authorized
+                v.update({'app_key': di.app_key, 'app_secret': di.app_secret})
+                sync_datastore()
+                now_est = datetime.now().replace(tzinfo=pytz.timezone('America/New_York'))
+                message = 'synced @ %s' % (now_est.strftime('%I:%M:%S%p %Z'))
+                v['sync'] =  message
+                render.page(self, html_template, values=v)
+
 
 
 
@@ -92,6 +84,7 @@ class main(webapp2.RequestHandler):
 
 class handshake(webapp2.RequestHandler):
     def get(self, mode="login"):
+        v = {}
         if mode == 'login':
             callback = self.request.host_url + '/sync/initialize'
             dropbox = dropbox_api(callback)
@@ -114,8 +107,7 @@ class handshake(webapp2.RequestHandler):
             for p in paths:
                 params = {'root': 'sandbox', 'path': p}
                 api_request = dropbox.create_folder(params)
-            sync_datastore()
             self.redirect('/dropbox')
-
-
+        if mode == 'upload':
+            dropbox = dropbox_api()
 
